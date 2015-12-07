47a9e47e6c9e9c2f1ff382463a33ba44f5962943
Krzysztof Laskowski <krzysztof.laskowski@nsn.com>
2015-12-07 11:05:33 +0100

    omit frame ptr during profiling

--- cpp-bench/1/compile_bench.sh
+++ cpp-bench/1/compile_bench.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++ -DGOOGLE_BENCHMARK -DNDEBUG -Wall -Werror -std=c++14 -O3 main.cpp -lbenchmark -lpthread
+g++ -DGOOGLE_BENCHMARK -DNDEBUG -Wall -Werror -std=c++14 -O3 -fno-omit-frame-pointer main.cpp -lbenchmark -lpthread
