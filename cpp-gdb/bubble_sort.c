d02ea63be0e671aa5399308b31fa68302b90fdcb
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2016-08-06 18:07:03 +0200

    bubble_sort reverse debug

---
+++ cpp-gdb/bubble_sort.c
@@ -0,0 +1,42 @@
+#include <time.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+void sort(long* array)
+{
+    int i = 0;
+    bool sorted;
+
+    do {
+        sorted = true;
+        for (i = 0; i < 31; i++) {
+            long* item_one = &array[i];
+            long* item_two = &array[i+1];
+            long swap_store;
+
+            if (*item_one <= *item_two) {
+                continue;
+            }
+
+            sorted = false;
+            swap_store = *item_two;
+            *item_two = *item_one;
+            *item_one = swap_store;
+        }
+    } while (!sorted);
+}
+
+int main()
+{
+    long array[32];
+    int i = 0;
+
+    srand(time(NULL));
+    for (i = 0; i < rand() % sizeof array; i++) {
+        array[i] = rand();
+    }
+
+    sort(array);
+
+    return 0;
+}
