45928f9fe843112988fc137328d80980d93869e0
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-01-14 18:46:11 +0100

    reproduction of lib py make_numbered_dir race bug

---
+++ python-pyrace/test_repro.py
@@ -0,0 +1,21 @@
+import os
+import py
+import pytest
+
+@pytest.yield_fixture(scope='session')
+def rootdir():
+    yield py.path.local(os.path.join(os.path.dirname(__file__), 'rootdir'))
+
+@pytest.mark.parametrize('_', 100 * [''])
+def test_reproduce(rootdir, _):
+    for i in range(100):
+        tmpdir = py.path.local.make_numbered_dir(prefix='repro-', rootdir=rootdir)
+        tmpdir.join('foo').write('bar')
+        assert tmpdir.join('foo').read() == 'bar'
+        tmpdir.remove()
+
+# run this suite with xdist to reproduce:
+# $ pytest -n10
+
+# this line of code put after 'udir = rootdir.mkdir(prefix + str(maxnum+1))' increases chances of reproduction greatly:
+# import time,random;time.sleep(random.random()/100)
