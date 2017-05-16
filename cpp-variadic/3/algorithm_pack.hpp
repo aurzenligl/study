cedfd1dbc1adec024a008e4ce69068f205ab6804
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-05-16 22:37:24 +0200

    argument pack usage - for_args_if, meta_count_if, is_specialization

---
+++ cpp-variadic/3/algorithm_pack.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+template<typename Test, template<typename...> class Ref>
+struct is_specialization : std::false_type {};
+
+template<template<typename...> class Ref, typename... Args>
+struct is_specialization<Ref<Args...>, Ref>: std::true_type {};
+
+template <template<class> class MetaPred, typename... Args>
+struct meta_count_if;
+
+template <template<class> class MetaPred>
+struct meta_count_if<MetaPred>
+{
+    enum { value = 0 };
+};
+
+template <template<class> class MetaPred, typename Arg, typename... Args>
+struct meta_count_if<MetaPred, Arg, Args...>
+{
+    enum { value = MetaPred<Arg>::value + meta_count_if<MetaPred, Args...>::value };
+};
+
+template <template<class> class MetaPred, typename UnaryFun>
+void for_args_if(UnaryFun fun)
+{}
+
+template <template<class> class MetaPred, typename UnaryFun, typename Arg, typename... Args>
+void for_args_if(UnaryFun fun, Arg&& arg, Args&&... args)
+{
+    if (MetaPred<Arg>::value)
+    {
+        fun(std::forward<Arg>(arg));
+    }
+    for_args_if<MetaPred>(fun, std::forward<Args>(args)...);
+}
---
+++ cpp-variadic/3/main.cpp
@@ -0,0 +1,32 @@
+#include "algorithm_pack.hpp"
+#include <iostream>
+#include <vector>
+
+struct Printer
+{
+    template <typename T>
+    void operator()(const T& t)
+    {
+        std::cout << t << '\n';
+    }
+};
+
+int main()
+{
+    std::cout
+        << "count arithmetic: "
+        << meta_count_if<std::is_arithmetic, int, const char*, unsigned, std::string, float>::value << '\n'
+        << "count literal: "
+        << meta_count_if<std::is_literal_type, int, const char*, unsigned, std::string, float>::value << '\n';
+
+    for_args_if<std::is_arithmetic>(Printer(), 1, "foo", 2u, std::string("bar"), 3.f);
+    for_args_if<std::is_literal_type>(Printer(), 1, "foo", 2u, std::string("bar"), 3.f);
+
+    std::cout
+        << "std::vector<int>: "
+        << is_specialization<std::vector<int>, std::vector>::value << '\n'
+        << "int: "
+        << is_specialization<int, std::vector>::value << '\n'
+        << "pair<int, int>: "
+        << is_specialization<std::pair<int, int>, std::pair>::value << '\n';
+}
