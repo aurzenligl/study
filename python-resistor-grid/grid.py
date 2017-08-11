16872cf8be05aef1121d813a71de3f74045ac108
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2017-08-11 05:00:42 +0200

    graph is graphvizzable now

--- python-resistor-grid/grid.py
+++ python-resistor-grid/grid.py
@@ -15,37 +15,102 @@ class Graphizer:
         g.render(filename='%s.%03d' % (self.name, self.count))
         self.count += 1
     def from_graph(self, graph):
-        nodes = ['%s:%s' % (node.loc.x, node.loc.y) for node in graph.nodes]
-        self(nodes, [])
+        nodes = [node.name for node in graph.nodes]
+        edges = [(neigh.node.name, node.name) for node in graph.nodes for neigh in node.neighbors]
+        self(nodes, edges)
 
 class Loc:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def __eq__(self, rhs):
+        return self.x == rhs.x and self.y == rhs.y
+
+class Edge:
+    def __init__(self, value):
+        self.value = value
+
+class Neighbor:
+    def __init__(self, node, edge):
+        self.node = node
+        self.edge = edge
 
 class Node:
     def __init__(self, loc):
         self.loc = loc
-        self.nodes = []
+        self.neighbors = []
+    @property
+    def name(self):
+        return '%s,%s' % (self.loc.x, self.loc.y)
 
 class Graph:
     def __init__(self):
         self.nodes = []
+    def find(self, x, y):
+        return next((node for node in self.nodes if node.loc == Loc(x, y)), None)
     def add(self, x, y):
-        self.nodes.append(Node(Loc(x, y)))
-
-# 01 11 21
-# 00 10 20
+        node = Node(Loc(x, y))
+        self.nodes.append(node)
+        self._link(node)
+    def _link(self, node):
+        candidates = [
+            self.find(node.loc.x+1, node.loc.y),
+            self.find(node.loc.x-1, node.loc.y),
+            self.find(node.loc.x, node.loc.y+1),
+            self.find(node.loc.x, node.loc.y-1),
+        ]
+        for cand in candidates:
+            if cand:
+                edge = Edge(1)
+                cand.neighbors.append(Neighbor(node, edge))
+                node.neighbors.append(Neighbor(cand, edge))
 
 g = Graphizer('snapshot')
+
+# TODO loop over making nodes
+# TODO label edge values
+# TODO single edges instead of doubles
 
 x = Graph()
 x.add(0, 0)
 x.add(0, 1)
 x.add(1, 0)
-
+x.add(1, 1)
 g.from_graph(x)
 
+x = Graph()
+x.add(0, 0)
+x.add(0, 1)
+x.add(1, 0)
+x.add(1, 1)
+x.add(2, 0)
+x.add(2, 1)
+g.from_graph(x)
+
+x = Graph()
+x.add(-1, -1)
+x.add(-1, 0)
+x.add(-1, 1)
+x.add(-1, 2)
+x.add(0, -1)
+x.add(0, 0)
+x.add(0, 1)
+x.add(0, 2)
+x.add(1, -1)
+x.add(1, 0)
+x.add(1, 1)
+x.add(1, 2)
+x.add(2, -1)
+x.add(2, 0)
+x.add(2, 1)
+x.add(2, 2)
+x.add(3, -1)
+x.add(3, 0)
+x.add(3, 1)
+x.add(3, 2)
+g.from_graph(x)
+
+'''
 g([
     'A',
     'B',
@@ -70,4 +135,5 @@ g([
     ('D', 'B'),
     ('D', 'A'),
 ])
+'''
 
