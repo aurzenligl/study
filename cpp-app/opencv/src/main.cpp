008ad4227813e53e5ac6dbb4fd16f132581aac29
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2018-01-12 18:41:30 +0100

    clara vendored in, used instead of boost cmdline parser

--- cpp-app/opencv/src/main.cpp
+++ cpp-app/opencv/src/main.cpp
@@ -1,36 +1,73 @@
 #include <iostream>
-#include "opts.hpp"
+#include <string>
 #include "process.hpp"
+#include "clara/clara.hpp"
+#include <sys/stat.h>
+
+inline bool exists(const std::string& filename)
+{
+    struct stat buffer;
+    return stat(filename.c_str(), &buffer) == 0;
+}
+
+auto check_file = [](std::string& out)
+{
+    return [&](const std::string& str)
+    {
+        if (!exists(str))
+        {
+            return clara::ParserResult::runtimeError("Expected existing file, got: '" + str + "'");
+        }
+        out = str;
+        return clara::ParserResult::ok(clara::ParseResultType::Matched);
+    };
+};
 
 int main(int ac, char* av[])
 {
-    // TODO: use clara as cmdline parser
+    bool help = false;
+    int operation = 0;
+    std::string filename;
 
-    app::cmdline_opts opts;
-    if (app::cmdline_status st = app::parse(ac, av, opts))
+    auto cli
+        = clara::Help(help)
+        | clara::Opt(operation, "operation")
+            ["-o"]["--oper"]
+            ("operation to apply to image")
+        | clara::Arg(check_file(filename), "image file")
+            ("image file");
+
+    auto result = cli.parse(clara::Args(ac, av));
+    if (!result)
     {
-        return st.return_code;
+        std::cerr << "Error in command line: " << result.errorMessage() << std::endl;
+        exit(1);
     }
 
-    std::cout << "Operation " << opts.operation << " set.\n";
-    std::cout << "File " << opts.filename << " set.\n";
+    if (help or filename.empty())
+    {
+        std::cout << cli;
+    }
 
-    switch (opts.operation)
+    std::cout << "Operation " << operation << " set.\n";
+    std::cout << "File " << filename << " set.\n";
+
+    switch (operation)
     {
     case 0:
-        app::show_image(opts.filename);
+        app::show_image(filename);
         break;
     case 1:
-        app::convert_image(opts.filename);
+        app::convert_image(filename);
         break;
     case 2:
-        app::reduce_color_space(opts.filename);
+        app::reduce_color_space(filename);
         break;
     case 3:
-        app::sharpen(opts.filename);
+        app::sharpen(filename);
         break;
     default:
-        std::cout << "Operation " << opts.operation << " has no action.\n";
+        std::cout << "Operation " << operation << " has no action.\n";
         break;
     }
 }
