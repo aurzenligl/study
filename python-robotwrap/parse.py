f776e1c61d55ea2520add980550fe7d24412d207
Marek Jarycki <marek.jarycki@nsn.com>
2018-01-23 10:56:45 +0100

    decorator insertion

--- python-robotwrap/parse.py
+++ python-robotwrap/parse.py
@@ -32,9 +32,25 @@ def find_functions(node):
         fun.parent = node
     return funs
 
+def insert_decorators(path, locs):
+    with open(path, 'rw+') as f:
+        lines = f.readlines()
+        already_inserted = 0
+        for loc in locs:
+            line, offset = loc
+            pill = ' ' * offset + '@trxstub\n'
+            lines.insert(line + already_inserted -1, pill)
+            already_inserted += 1
+        f.seek(0)
+        f.truncate()
+        f.write(''.join(lines))
+
 def to_locname(node):
     path = '::'.join([p.name for p in get_path(node)])
     return '%s:%s:%s' % (path, node.lineno, node.col_offset)
+
+def to_locname2(path, locs):
+    insert_decorators(path, locs)
 
 def sort_by_line(nodes):
     return sorted(nodes, key=lambda x: x.lineno)
@@ -49,7 +65,9 @@ def file_to_locs(filepath):
     funs = find_functions(tree)
     meths = find_functions(find_class(tree, to_purebasename(filepath)))
 
-    return map(to_locname, sort_by_line(funs + meths))
+    keywords = sort_by_line(funs + meths)
+    insert_decorators(filepath, [(node.lineno, node.col_offset) for node in keywords])
+    return map(to_locname, keywords)
 
 def main():
     args = sys.argv[1:]
