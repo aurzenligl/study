59f12e49f7d716906fb94870d3af9918a5955c17
Marek Jarycki <marek.jarycki@nsn.com>
2018-01-23 11:06:26 +0100

    cleanup

--- python-robotwrap/parse.py
+++ python-robotwrap/parse.py
@@ -10,15 +10,6 @@ def is_fun(node):
 def is_class(node):
     return isinstance(node, ast.ClassDef)
 
-def get_path(node):
-    path = [node]
-    while True:
-        node = getattr(node, 'parent', None)
-        if node:
-            path.insert(0, node)
-        else:
-            return path
-
 def find_class(node, name):
     for child in ast.iter_child_nodes(node):
         if is_class(child) and child.name == name:
@@ -32,30 +23,23 @@ def find_functions(node):
         fun.parent = node
     return funs
 
-def insert_decorators(path, locs):
+def insert(decorator, path, locs):
     with open(path, 'rw+') as f:
         lines = f.readlines()
         already_inserted = 0
         for loc in locs:
             line, offset = loc
-            pill = ' ' * offset + '@trxstub\n'
+            pill = ' ' * offset + decorator + '\n'
             lines.insert(line + already_inserted -1, pill)
             already_inserted += 1
         f.seek(0)
         f.truncate()
         f.write(''.join(lines))
 
-def to_locname(node):
-    path = '::'.join([p.name for p in get_path(node)])
-    return '%s:%s:%s' % (path, node.lineno, node.col_offset)
-
-def to_locname2(path, locs):
-    insert_decorators(path, locs)
-
 def sort_by_line(nodes):
     return sorted(nodes, key=lambda x: x.lineno)
 
-def file_to_locs(filepath):
+def process_file(filepath):
     tree = ast.parse(open(filepath).read())
     tree.name = filepath
 
@@ -65,17 +49,15 @@ def file_to_locs(filepath):
     funs = find_functions(tree)
     meths = find_functions(find_class(tree, to_purebasename(filepath)))
 
-    keywords = sort_by_line(funs + meths)
-    insert_decorators(filepath, [(node.lineno, node.col_offset) for node in keywords])
-    return map(to_locname, keywords)
+    locs = [(node.lineno, node.col_offset) for node in sort_by_line(funs + meths)]
+    insert('@trxstub', filepath, locs)
 
 def main():
     args = sys.argv[1:]
     if len(args) != 1:
         sys.exit('Usage: parse.py <filename>')
     filepath = sys.argv[1]
-    for loc in file_to_locs(filepath):
-        print loc
+    process_file(filepath)
 
 if __name__ == '__main__':
     main()
