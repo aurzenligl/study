032da511ef200f0157d687ad0974e589291aaec3
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2018-01-28 20:46:50 +0100

    simplification, no need to use classmethod instead of staticmethod, it's enough to capture instantiating class at __get__ time

--- python-robotwrap/sitecustomize.py
+++ python-robotwrap/sitecustomize.py
@@ -1,34 +1,19 @@
-def to_cls(f):
-    cls = getattr(f, 'im_class', None)
-    if cls == type:
-        cls = f.im_self
-    return cls
-
-def to_path(f):
-    cls = to_cls(f)
-    physical_path = '%s:%s' % (f.func_code.co_filename, f.func_code.co_firstlineno)
-    logical_path = (cls.__name__ + '.' if cls else '') + f.func_name
+def to_path(fun, cls):
+    physical_path = '%s:%s' % (fun.func_code.co_filename, fun.func_code.co_firstlineno)
+    logical_path = (cls.__name__ + '.' if cls else '') + fun.func_name
     return '%s %s' % (physical_path, logical_path)
 
 class decorated(object):
-    def __init__(self, func):
-        if isinstance(func, staticmethod):
-            func.__func__.is_static = True
-            self.func = classmethod(func.__func__)
-        else:
-            self.func = func
+    def __init__(self, func, type_=None):
+        self.func = func
+        self.type = type_
 
-    def __get__(self, obj, type=None):
-        return self.__class__(self.func.__get__(obj, type))
+    def __get__(self, obj, type_=None):
+        return self.__class__(self.func.__get__(obj, type_), type_)
 
     def __call__(self, *args, **kwargs):
-        print to_path(self.func), args, kwargs
-
-        if getattr(self.func, 'is_static', None):
-            f = self.func.__func__
-        else:
-            f = self.func
-        return f(*args, **kwargs)
+        print to_path(self.func, self.type), args, kwargs
+        return self.func(*args, **kwargs)
 
 import __builtin__
 
