684676fee59e7827015a82f59d24646795818b1b
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2018-01-28 21:16:38 +0100

    little refactoring

--- python-robotwrap/Example4.py
+++ python-robotwrap/Example4.py
@@ -19,7 +19,9 @@ class decorated(object):
     You may access class at call time.
     '''
     def __call__(self, *args, **kwargs):
-        print('called %s.%s with args=%s kwargs=%s' % (self.type.__name__, self.func.__name__, args, kwargs))
+        clsname = self.type.__name__
+        funcname = self.func.__name__
+        print('called %s.%s with args=%s kwargs=%s' % (clsname, funcname, args, kwargs))
         return self.func(*args, **kwargs)
 
 class Foo(object):
