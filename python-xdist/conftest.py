20bed7573acbb6804e95131cbd22661da68b364c
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2018-02-18 23:09:46 +0100

    cleanup of unused parts, encapsulation of pyro-fixture engine parts

--- python-xdist/conftest.py
+++ python-xdist/conftest.py
@@ -10,7 +10,7 @@ def do_the_long_thing():
     put('LONGTHING.start %.4f' % (time.time()))
     time.sleep(0.5)
     put('LONGTHING.end %.4f' % (time.time()))
-    return '<long thing result>'
+    return 42
 
 @pytest.fixture(scope='session')
 def sfix(request):
@@ -40,62 +40,57 @@ def pytest_addoption(parser):
                      help='Dummy variable just for the heck of it')
 
 def pytest_logger_logdirlink(config):
-    #put('HOOK.pytest_logger_logdirlink')
     return os.path.join(os.path.dirname(__file__), 'logs')
 
 def pytest_logger_config(logger_config):
-    #put('HOOK.pytest_logger_config')
     logger_config.add_loggers(['setup'], stdout_level='info')
     logger_config.set_log_option_default('setup')
 
 @Pyro4.expose
-class GreetingMaker(object):
+class TestrunFixtureEngine(object):
     def __init__(self):
-        self.pq = Queue()
-        self.pq.put(True)
-        self.q = Queue()
+        self.produce_token = Queue()
+        self.produce_token.put(True)
+        self.results = Queue()
     def should_produce(self):
         try:
-            return self.pq.get(False)
+            return self.produce_token.get(False)
         except Empty:
             pass
     def get(self):
-        return self.q.get()
+        return self.results.get()
     def put(self, x):
-        self.q.put(x)
-    def name(self):
-        return "the name"
-    def get_fortune(self, name):
-        return "Hello, {0}. Here is your fortune message:\n" \
-               "Behold the warranty -- the bold print giveth and the fine print taketh away.".format(name)
+        self.results.put(x)
 
 def pytest_configure_node(node):
     put('HOOK.pytest_configure_node')
+    in_firstnode(node, lambda: setup_daemon(node))
+    node.slaveinput['uri'] = node.config.firstnode.slaveinput['uri']
+
+def pytest_testnodedown(node, error):
+    in_firstnode(node, lambda: node.pyro.close())
+    put('HOOK.pytest_testnodedown')
+
+def pytest_runtest_protocol(item, nextitem):
+    session = item.session
+    put('HOOK.pytest_runtest_protocol %s' % session.config.slaveinput)
+
+#######
+
+def in_firstnode(node, fun):
     if getattr(node.config, 'firstnode', None) is None:
         node.config.firstnode = node
     if node.config.firstnode is node:
-        daemon, uri = setup_daemon()
-        node.pyro = daemon
-        node.slaveinput['uri'] = str(uri)
-    else:
-        node.slaveinput['uri'] = node.config.firstnode.slaveinput['uri']
+        fun()
 
-def setup_daemon():
+def setup_daemon(node):
     daemon = Pyro4.Daemon()
-    uri = daemon.register(GreetingMaker())
+    uri = daemon.register(TestrunFixtureEngine())
 
     from threading import Thread
     thr = Thread(target=daemon.requestLoop)
     thr.daemon = True
     thr.start()
 
-    return daemon, uri
-
-def pytest_testnodedown(node, error):
-    if node.config.firstnode is node:
-        node.pyro.close()
-    put('HOOK.pytest_testnodedown')
-
-def pytest_runtest_protocol(item, nextitem):
-    session = item.session
-    put('HOOK.pytest_runtest_protocol %s' % session.config.slaveinput)
+    node.pyro = daemon
+    node.slaveinput['uri'] = str(uri)
