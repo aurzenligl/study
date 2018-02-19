9a2e85bc1e1124c928ea10d5f9db1c7ba1480141
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2018-02-19 20:54:53 +0100

    logged module has no quirky underscores, file-writing part of file is separated

--- python-logger/thelib/logged.py
+++ python-logger/thelib/logged.py
@@ -14,26 +14,27 @@ def logged(fun):
 
     @wraps(fun)
     def wrap(*args, **kwargs):
-        cls, nonselfargs = _declassify(fun, args)
+        cls, nonselfargs = declassify(fun, args)
 
         if _logging.currently:
             return fun(*args, **kwargs)
 
         with _logging:
-            for line in _func_call_to_lines(fun, cls, nonselfargs, kwargs):
-                logger.info(line)
-            with _Timed() as t:
+            for line in func_call_to_lines(fun, cls, nonselfargs, kwargs):
+                output(line)
+            with Timed() as t:
                 ret = fun(*args, **kwargs)
-            for line in _func_ret_to_lines(t.duration, ret):
-                logger.info(line)
+            for line in func_ret_to_lines(t.duration, ret):
+                output(line)
             return ret
+
     wrap.wrapped = fun
 
     if desc:
         wrap = desc(wrap)
     return wrap
 
-def _declassify(fun, args):
+def declassify(fun, args):
     if len(args):
         met = getattr(args[0], fun.__name__, None)
         if met:
@@ -46,33 +47,37 @@ def _declassify(fun, args):
                     return cls, args[1:]
     return None, args
 
-def _func_call_to_lines(fun, cls, args, kwargs):
-    return [_to_path(fun, cls)] + _indent(_flatten_lines(
+def output(line):
+    logger.info(line)
+    write_to_file(line)
+
+def func_call_to_lines(fun, cls, args, kwargs):
+    return [to_path(fun, cls)] + indent(flatten_lines(
         ['%s: %s' % (i, pprint.pformat(a)) for i, a in enumerate(args)] +
         ['%s: %s' % (k, pprint.pformat(v)) for k, v in kwargs.items()]
     ))
 
-def _func_ret_to_lines(duration, ret):
-    return _indent(_flatten_lines(['-> (%.2fs) %s' % (duration, pprint.pformat(ret))]))
+def func_ret_to_lines(duration, ret):
+    return indent(flatten_lines(['-> (%.2fs) %s' % (duration, pprint.pformat(ret))]))
 
-def _flatten_lines(lines):
+def flatten_lines(lines):
     out = []
     for line in lines:
         rawlines = line.splitlines()
         out.append(rawlines[0])
-        out.extend(_indent(rawlines[1:]))
+        out.extend(indent(rawlines[1:]))
     return out
 
-def _to_path(fun, cls):
+def to_path(fun, cls):
     physical_path = fun.__module__.replace('.', '/') + '.py:%s' % fun.__code__.co_firstlineno
     logical_path = (cls.__name__ + '.' if cls else '') + fun.func_name
     return '%s %s' % (physical_path, logical_path)
 
-def _indent(lines):
+def indent(lines):
     indent = ' ' * 4
     return [indent + line for line in lines]
 
-class _Timed:
+class Timed:
     def __enter__(self):
         self.start = time.time()
         return self
@@ -81,7 +86,7 @@ class _Timed:
         end = time.time()
         self.duration = end - self.start
 
-class _Logging:
+class Logging:
     def __init__(self):
         self.currently = False
 
@@ -91,4 +96,25 @@ class _Logging:
     def __exit__(self, *_):
         self.currently = False
 
-_logging = _Logging()
+_logging = Logging()
+
+def maketmp():
+    import os
+    try:
+        os.mkdir('/tmp/luger')
+    except OSError:
+        pass
+
+def write_to_file(line):
+    import os
+    with open('/tmp/luger/%s' % os.getpid(), 'a') as f:
+        relnow = time.time() - _import_time
+        f.write('{0:08.3f}: {1}\n'.format(relnow, line))
+
+def output_cmdline():
+    import sys
+    output('cmdline args: %s' % sys.argv)
+
+_import_time = time.time()
+maketmp()
+output_cmdline()
