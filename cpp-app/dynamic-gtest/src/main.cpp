aba9c1af33b813e6b804b0191c67837d3c015468
Krzysztof Laskowski <krzysztof.laskowski@nokia.com>
2018-04-29 17:30:00 +0200

    project skeleton prepared for dynamic parametric tests project

---
+++ cpp-app/dynamic-gtest/src/main.cpp
@@ -0,0 +1,6 @@
+#include "app.hpp"
+
+int main()
+{
+    app::bar(app::foo(42));
+}
---
+++ cpp-app/dynamic-gtest/test/foo/test_foo.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <app.hpp>
+
+using namespace testing;
+
+TEST(FooTest, foo_null)
+{
+    EXPECT_EQ(0, app::foo(0));
+}
+
+TEST(FooTest, foo_42)
+{
+    EXPECT_EQ(42, app::foo(42));
+}
