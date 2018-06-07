5e541a4430798c55880a75aa3c757972794148f7
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-06-07 14:10:37 +0200

    gold linker extracted from commands - without it program works fine

--- cpp-pthread/compile.sh
+++ cpp-pthread/compile.sh
@@ -4,10 +4,13 @@
 
 [ "$CXX" ] || CXX=g++
 
+GOLD="-fuse-ld=gold"
+#GOLD="" without gold linker it works
+
 mkdir -p build
 
-$CXX -Dodr_EXPORTS -I. -fdiagnostics-color -fuse-ld=gold -O2 -g -DNDEBUG -fPIC   -Wall -fPIC -Werror -std=gnu++11 -o build/odr.cpp.o -c odr.cpp
-$CXX -fPIC -fdiagnostics-color -fuse-ld=gold -O2 -g -DNDEBUG  -shared -Wl,-soname,libodr.so -o build/libodr.so build/odr.cpp.o  -lpthread
+$CXX -Dodr_EXPORTS -I. -fdiagnostics-color $GOLD -O2 -g -DNDEBUG -fPIC   -Wall -fPIC -Werror -std=gnu++11 -o build/odr.cpp.o -c odr.cpp
+$CXX -fPIC -fdiagnostics-color $GOLD -O2 -g -DNDEBUG  -shared -Wl,-soname,libodr.so -o build/libodr.so build/odr.cpp.o  -lpthread
 
-$CXX -I. -fdiagnostics-color -fuse-ld=gold -O2 -g -DNDEBUG   -Wall -fPIC -Werror -std=gnu++11 -o build/test.cpp.o -c test.cpp
-$CXX -fdiagnostics-color -fuse-ld=gold -O2 -g -DNDEBUG  build/test.cpp.o -rdynamic build/libodr.so -pthread -lpthread -Wl,-rpath,build
+$CXX -I. -fdiagnostics-color $GOLD -O2 -g -DNDEBUG   -Wall -fPIC -Werror -std=gnu++11 -o build/test.cpp.o -c test.cpp
+$CXX -fdiagnostics-color $GOLD -O2 -g -DNDEBUG  build/test.cpp.o -rdynamic build/libodr.so -pthread -lpthread -Wl,-rpath,build
