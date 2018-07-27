79ac242b90776721725187ee92481e9ee7d49348
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-07-27 02:14:39 +0200

    yet another approach...

---
+++ cpp-overload-set/5-better-still.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <memory>
+#include <type_traits>
+
+template <typename From, typename To>
+struct EnableIfPointersConvert : std::enable_if<std::is_convertible<std::shared_ptr<From>, std::shared_ptr<To>>::value>
+{};
+
+template <typename T>
+void Foo(const T&) { std::cout << "value" << '\n'; }
+
+template <typename T, typename MaybeT, typename = typename EnableIfPointersConvert<MaybeT, T>::type>
+void Foo(std::shared_ptr<MaybeT>) { std::cout << "shptr" << '\n'; }
+
+struct B { virtual void f() = 0; };
+struct X : B { void f() override {} };
+using XPtr = std::shared_ptr<X>;
+
+int main() {
+  X ref;
+  XPtr shref = std::make_shared<X>();
+  const X &cref = X();
+  const XPtr& shcref = std::make_shared<X>();
+
+  Foo<X>({});  // value
+  Foo<X>(X());  // value
+  Foo<X>(ref);  // value
+  Foo<X>(cref);  // value
+  Foo<X>(std::make_shared<X>());  // shared
+  Foo<X>(shref);  // shared
+  Foo<X>(shcref);  // shared
+
+  Foo<B>(std::make_shared<X>());  // shared
+  Foo<B>(shref);  // shared
+  Foo<B>(shcref);  // shared  
+
+  Foo<XPtr>({});  // value
+  Foo<XPtr>(std::make_shared<X>());  // value
+  Foo<XPtr>(XPtr());  // value
+  Foo<XPtr>(shref);  // value
+  Foo<XPtr>(shcref);  // value
+}
