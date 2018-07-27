b6f605a87cb012d9058741d4a7fd0a232b60a1df
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-07-27 02:29:01 +0200

    approach 2, although simple, doesn't support shared_ptr conversions!

--- cpp-overload-set/2-implicit-conversion.cpp
+++ cpp-overload-set/2-implicit-conversion.cpp
@@ -13,10 +13,13 @@ void Foo(const T&) { std::cout << "value" << '\n'; }
 template <typename T>
 void Foo(Wrap<std::shared_ptr<T>>) { std::cout << "shptr" << '\n'; }
 
-struct X{};
+struct B { virtual void f() = 0; };
+struct X : B { void f() override {} };
 
 int main() {
-  Foo<X>({});
+  Foo<X>({});  // this works nicely, shared_ptr overload is a worse match, as it should!
   Foo<X>(X());
   Foo<X>(std::make_shared<X>());
+
+  // Foo<B>(std::make_shared<X>());  // but this doesn't work!
 }
