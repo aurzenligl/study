fd594d4cd000462382c1867495c3403ed773adf1
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-08-06 18:08:23 +0200

    cleaning script works properly regardless of cwd

--- ros-distro/clean.bash
+++ ros-distro/clean.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rm -rf ./build ./devel ./install ./src/ros-kinetic-* ./src/*/debian ./src/*/obj-x86_64-linux-gnu
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+rm -rf ${DIR}/build ${DIR}/devel ${DIR}/install ${DIR}/src/ros-kinetic-* ${DIR}/src/*/debian ${DIR}/src/*/obj-x86_64-linux-gnu
