8c295b95046e5d1ab2f1f10ef828757ed915057a
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-08-08 23:07:19 +0000

    fixing fetch script

--- debian-mongo/fetch.bash
+++ debian-mongo/fetch.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget https://github.com/mongodb/mongo-cxx-driver/archive/r3.3.1.tar.gz -O mongo-cxx-driver_3.3.1.orig.tar.gz
+wget https://github.com/mongodb/mongo-cxx-driver/archive/r3.1.1.tar.gz
+tar xf r3.1.1.tar.gz
