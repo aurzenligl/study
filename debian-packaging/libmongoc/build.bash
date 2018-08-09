3f340dfb8b633045c424d217be181bf12d49bbde
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-08-09 15:37:43 +0000

    -i option not needed

--- debian-packaging/libmongoc/build.bash
+++ debian-packaging/libmongoc/build.bash
@@ -3,4 +3,4 @@
 git clone --branch 1.6.3 --depth 1 --recurse-submodules https://github.com/mongodb/mongo-c-driver.git
 cd mongo-c-driver
 ln -s ../debian
-debuild -rfakeroot -i -us -uc -b -j`nproc`
+debuild -rfakeroot -us -uc -b -j`nproc`
