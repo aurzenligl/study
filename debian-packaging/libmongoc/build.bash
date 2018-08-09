a0d4911320db957079f29aa9ebfd6b659f62e746
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-08-09 13:41:26 +0000

    build.bash script fetches sources and builds package(s)

---
+++ debian-packaging/libmongoc/build.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone --branch 1.6.3 --depth 1 --recurse-submodules https://github.com/mongodb/mongo-c-driver.git
+cd mongo-c-driver
+ln -s ../debian
+debuild -rfakeroot -i -us -uc -b -j`nproc`
--- debian-packaging/libmongoc/fetch.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/mongodb/mongo-c-driver/archive/1.6.3.tar.gz
-tar xf 1.6.3.tar.gz
