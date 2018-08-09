90cccbe38befe37c528b95aa85b69226021eafae
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-08-09 20:55:49 +0000

    download much newer PCL version, call it a "dev1" version

--- debian-packaging/libpcl/build.bash
+++ debian-packaging/libpcl/build.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-git clone --branch pcl-1.8.1 --depth 1 https://github.com/PointCloudLibrary/pcl.git
+git clone -n https://github.com/PointCloudLibrary/pcl.git
 cd pcl
+git checkout b0b7fa4b49e4c9034f0404c8143f614684940f  # some frozen commit near master chosen for no particular reason
 ln -s ../debian
 debuild -rfakeroot -us -uc -b -j`nproc`
