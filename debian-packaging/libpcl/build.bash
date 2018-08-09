7399ee49562b4b2047b7b15532633839df2b6aea
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2018-08-09 20:56:33 +0000

    j`nproc` is not necessary

--- debian-packaging/libpcl/build.bash
+++ debian-packaging/libpcl/build.bash
@@ -4,4 +4,4 @@ git clone -n https://github.com/PointCloudLibrary/pcl.git
 cd pcl
 git checkout b0b7fa4b49e4c9034f0404c8143f614684940f  # some frozen commit near master chosen for no particular reason
 ln -s ../debian
-debuild -rfakeroot -us -uc -b -j`nproc`
+debuild -rfakeroot -us -uc -b
