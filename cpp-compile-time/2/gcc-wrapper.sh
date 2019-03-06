1b79089dbcb593808d6bf97724cc88fbeec101f2
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2019-03-06 03:21:06 +0100

    gcc-wrapper produces only time, but measures includes compilation time

--- cpp-compile-time/2/gcc-wrapper.sh
+++ cpp-compile-time/2/gcc-wrapper.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-objfile=$(echo "$@" | tr ' ' '\n' | grep -m1 "\.cpp\.o$")
-"$@" -time=${objfile}.time -fstats 2>${objfile}.fstats
+# extract paths (original and includes)
+input=$(echo "$@" | tr ' ' '\n' | grep -m1 "\.cpp$")
+output=$(echo "$@" | tr ' ' '\n' | grep -m1 "\.cpp\.o$")
+hinput=$(echo "$output" | sed 's@\(/[^/]*\).cpp.o$@\1.includes.cpp@g')
+houtput="${hinput}.o"
+
+# set commands (original and includes)
+cmd="$@"
+hcmd=$(echo "$@" | sed "s@$(echo ${input} | sed 's/\./\\./g')@${hinput}@g" | sed "s@${output}@${houtput}@g")
+
+# generate includes-only source file
+"$@" -H -E -fdirectives-only 2>&1 >/dev/null | grep -Po "^\. \K.*" | sed -e 's/^\(.*\)/#include "\1"/' > $hinput
+
+# compile modules (original and includes)
+$cmd -time=${output}.time
+$hcmd -time=${houtput}.time
