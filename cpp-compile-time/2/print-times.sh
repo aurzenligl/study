59bc1fa9cedc0c718b279913df19dde7c92454df
Krzysztof Laskowski <aurzenligl@gmail.com>
2019-03-07 00:44:55 +0100

    script improved by printing ratios

--- cpp-compile-time/2/print-times.sh
+++ cpp-compile-time/2/print-times.sh
@@ -4,6 +4,7 @@ logdir=$1
 
 function die() {
     echo "Usage: ./print-times.sh [logs-dir]"
+    echo "Outputs columns: <cpp-utime> <includes-utime> <header-utime> <includes-ratio> <header-ratio> <filename>"
     exit 1
 }
 
@@ -19,14 +20,21 @@ function record() {
     htimef="${basef}.includes.cpp.o.time"
     hhtimef="${basef}.header.cpp.o.time"
 
+    time=$(cat $timef)
+    htime=$(cat $htimef)
+    hhtime=$(cat $hhtimef)
+
+    rhtime=$(echo "100 * ${htime} / ${time}" | bc)
+    rhhtime=$(echo "100 * ${hhtime} / ${time}" | bc)
+
+    echo "$time $htime $hhtime $rhtime $rhhtime $srcfr"
+
     # echo $basef
     # echo $timef
     # echo $htimef
     # echo $hhtimef
     # echo $srcfa
     # echo $srcfr
-
-    echo $(cat $timef) $(cat $htimef) $(cat $hhtimef) ${srcfr}
 }
 
 export -f record
