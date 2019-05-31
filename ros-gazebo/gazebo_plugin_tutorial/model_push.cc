96cfd1c09e824cbcebafd05859fdd3a9ba106cf7
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2019-05-31 20:00:41 +0000

    model_push model plugin

---
+++ ros-gazebo/gazebo_plugin_tutorial/model_push.cc
@@ -0,0 +1,34 @@
+#include <functional>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <ignition/math/Vector3.hh>
+
+namespace gazebo {
+
+class ModelPush : public ModelPlugin {
+ public:
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr /*sdf*/) {
+    // Store the pointer to the model
+    this->model = _parent;
+
+    // Listen to the update event. This event is broadcast every
+    // simulation iteration.
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+        std::bind(&ModelPush::OnUpdate, this));
+  }
+
+  // Called by the world update start event
+  void OnUpdate() {
+    // Apply a small linear velocity to the model.
+    this->model->SetLinearVel(ignition::math::Vector3d(.3, 0, 0));
+  }
+
+ private:
+  physics::ModelPtr model;  // Pointer to the model
+  event::ConnectionPtr updateConnection;  // Pointer to the update event connection
+};
+
+GZ_REGISTER_MODEL_PLUGIN(ModelPush)  // Register this plugin with the simulator
+
+}  // namespace gazebo
---
+++ ros-gazebo/plugin.sh
@@ -0,0 +1,1 @@
+export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$(pwd)/gazebo_plugin_tutorial/build
