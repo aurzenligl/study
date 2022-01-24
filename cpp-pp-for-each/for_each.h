5f7d8b43987f13c14ada9f8f21e0746965c8a706
Krzysztof Laskowski <aurzenligl@gmail.com>
2022-01-24 02:35:58 +0100

    c++20 allows implementing preprocessor foreach

---
+++ cpp-pp-for-each/for_each.h
@@ -0,0 +1,24 @@
+// David Mazières, found in:
+// https://www.scs.stanford.edu/~dm/blog/va-opt.html#the-for_each-macro
+
+#define PARENS ()
+
+#define EXPAND(...) EXPAND4(EXPAND4(EXPAND4(EXPAND4(__VA_ARGS__))))
+#define EXPAND4(...) EXPAND3(EXPAND3(EXPAND3(EXPAND3(__VA_ARGS__))))
+#define EXPAND3(...) EXPAND2(EXPAND2(EXPAND2(EXPAND2(__VA_ARGS__))))
+#define EXPAND2(...) EXPAND1(EXPAND1(EXPAND1(EXPAND1(__VA_ARGS__))))
+#define EXPAND1(...) __VA_ARGS__
+
+#define FOR_EACH(macro, ...)                                    \
+  __VA_OPT__(EXPAND(FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define FOR_EACH_HELPER(macro, a1, ...)                         \
+  macro(a1)                                                     \
+  __VA_OPT__(FOR_EACH_AGAIN PARENS (macro, __VA_ARGS__))
+#define FOR_EACH_AGAIN() FOR_EACH_HELPER
+
+///////////////////////
+
+#define MEMBER(name) const int name;
+#define STRUCT(name, ...) struct name { FOR_EACH(MEMBER, __VA_ARGS__) };
+STRUCT(Foo, a, b, c, foo, bar, baz)
+
