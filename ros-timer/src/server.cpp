adf288edb86197b2cd88645fe38e598f4ac423e5
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2022-09-15 13:36:25 +0000

    foo::Timer draft...

--- ros-timer/src/server.cpp
+++ ros-timer/src/server.cpp
@@ -1,6 +1,7 @@
 #include <optional>
 #include <thread>
 #include <ros/ros.h>
+#include <ros/timer.h>
 #include <ros/timer_manager.h>
 #include <ros/callback_queue.h>
 #include <std_msgs/Int32.h>
@@ -8,6 +9,90 @@
 #include <sstream>
 #include <mutex>
 #include <foo/timeit.h>
+
+namespace foo {
+
+class Timer {
+ public:
+  Timer() = default;
+  Timer(const ros::Timer& rhs);
+  Timer& operator=(const ros::Timer& other);
+
+  // see ros::Timer for api documentation
+  void start();
+  void stop();
+  void blocking_stop();
+  bool hasPending();
+  void setPeriod(const ros::Duration &period, bool reset = true);
+  bool hasStarted() const;
+  bool isValid() const;
+  explicit operator bool() const;
+  bool operator<(const Timer& rhs);
+  bool operator==(const Timer& rhs);
+  bool operator!=(const Timer& rhs);
+
+ private:
+  struct Impl;
+  std::shared_ptr<Impl> impl_;
+};
+
+struct Timer::Impl {
+  Impl(const ros::Timer &timer) : timer(timer) {}
+  ~Impl() { blocking_stop(); }
+
+  void stop() {
+    // access to: ros::Timer::impl_
+    if (timer.impl_) {
+      int32_t thandle = timer.impl_->timer_handle_;
+      ROS_WARN_STREAM("aaa: " << thandle);
+
+      // rob to access: ros::TimerManager::timers_mutex_
+      // rob to access: ros::TimerManager::TimerInfoPtr
+      // rob to access: ros::TimerManager::findTimer
+      auto &mgr = ros::TimerManager<ros::Time, ros::Duration, ros::TimerEvent>::global();
+      if (auto info = (std::lock_guard(mgr.timers_mutex_), mgr.findTimer(thandle))) {
+        uint64_t removal_id = (uint64_t)info.get();
+        ROS_WARN_STREAM("aaa: " << removal_id);
+
+        // derive to access: ros::CallbackQueue::IDInfoPtr
+        // derive to access: ros::CallbackQueue::getIDInfo
+        ros::CallbackQueue& queue = *ros::getGlobalCallbackQueue();
+        id_info = queue.getIDInfo(removal_id);
+      }
+    }
+
+    (foo::Timeit("timer.stop()"), timer.stop());
+  }
+
+  void blocking_stop() {
+    ROS_WARN_STREAM("xxx: stopping...");
+    stop();
+    if (id_info) {
+      (foo::Timeit("calling_rw_mutex"), std::lock_guard(id_info->calling_rw_mutex));
+    }
+    ROS_WARN_STREAM("xxx: ...stopped");
+  }
+
+  ros::CallbackQueue::IDInfoPtr id_info;
+  ros::Timer timer;
+};
+
+Timer::Timer(const ros::Timer& rhs) : impl_(std::make_shared<Impl>(rhs)) {}
+Timer& Timer::operator=(const ros::Timer& rhs) { return *this = Timer(rhs); }
+
+void Timer::start() { if (impl_) impl_->timer.start(); }
+void Timer::stop() { if (impl_) impl_->stop(); }
+void Timer::blocking_stop() { if (impl_) impl_->blocking_stop(); }
+bool Timer::hasPending() { return impl_ ? impl_->timer.hasPending() : false; }
+void Timer::setPeriod(const ros::Duration &period, bool reset) { if (impl_) impl_->timer.setPeriod(period, reset); }
+bool Timer::hasStarted() const { return impl_ ? impl_->timer.hasStarted() : false; }
+bool Timer::isValid() const { return impl_ ? impl_->timer.isValid() : false; }
+Timer::operator bool() const { return isValid(); }
+bool Timer::operator<(const Timer& rhs) { return impl_ < rhs.impl_; }
+bool Timer::operator==(const Timer& rhs) { return impl_ == rhs.impl_; }
+bool Timer::operator!=(const Timer& rhs) { return impl_ != rhs.impl_; }
+
+}  // namespace foo
 
 struct Server {
   Server() {
@@ -22,25 +107,18 @@ struct Server {
   }
 
   void Stop() {
-    // access to: ros::Timer::impl_
-    int32_t thandle = timer.impl_->timer_handle_;
+    ROS_WARN_STREAM("xxx: running: " << timer.hasStarted());
+    timer.stop();
+    timer = foo::Timer();
 
-    // rob to access: ros::TimerManager::timers_mutex_
-    // rob to access: ros::TimerManager::TimerInfoPtr
-    // rob to access: ros::TimerManager::findTimer
-    auto &mgr = ros::TimerManager<ros::Time, ros::Duration, ros::TimerEvent>::global();
-    auto info = (std::lock_guard(mgr.timers_mutex_), mgr.findTimer(thandle));
-    uint64_t removal_id = (uint64_t)info.get();
+    foo::Timer x = timer;
+    foo::Timer y(timer);
+    y = x;
 
-    // derive to access: ros::CallbackQueue::IDInfoPtr
-    // derive to access: ros::CallbackQueue::getIDInfo
-    ros::CallbackQueue& queue = *ros::getGlobalCallbackQueue();
-    ros::CallbackQueue::IDInfoPtr id_info = queue.getIDInfo(removal_id);
-
-    ROS_WARN_STREAM("xxx: ids...: " << thandle << " " << removal_id << " " << id_info->id);
-    (foo::Timeit("timer.stop()"), timer.stop());
-    (foo::Timeit("calling_rw_mutex"), std::lock_guard(id_info->calling_rw_mutex));
-    ROS_WARN_STREAM("xxx: ...stopped");
+    timer < timer;
+    timer == timer;
+    timer != timer;
+    if (timer) {}
   }
 
   void OnTopic(std_msgs::Int32 x) {
@@ -64,7 +142,7 @@ struct Server {
 
   void OnTick(const ros::TimerEvent &event) {
     ROS_WARN_STREAM("xxx: OnTick tic...");
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    std::this_thread::sleep_for(std::chrono::seconds(1));
     ROS_WARN_STREAM("xxx: OnTick ...toc");
   }
 
@@ -72,7 +150,7 @@ struct Server {
   ros::Timer quick_timer1;
   ros::Timer quick_timer2;
   ros::Timer quick_timer3;
-  ros::Timer timer;
+  foo::Timer timer;
   ros::Subscriber sub;
   ros::ServiceServer srv;
   ros::ServiceServer stop;
