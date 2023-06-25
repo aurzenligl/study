56e573eca8606f05c806ea170a98fcd8e1b6a62f
Krzysztof Laskowski <aurzenligl@gmail.com>
2023-06-25 02:19:13 +0200

    cats-n-boxes -> yet another iteration

--- games/cats-n-boxes/test.py
+++ games/cats-n-boxes/test.py
@@ -39,60 +39,76 @@ class Gen:
     def a(x, y, th, fn):
         """
         a..
-        Aaa
+        Axa
         """
-        fn(x, y)
-        fn(*translate(x, y, *rotate(0, 1, th)))
-        fn(*translate(x, y, *rotate(1, 0, th)))
-        fn(*translate(x, y, *rotate(2, 0, th)))
+        fn(x, y, False)
+        fn(*translate(x, y, *rotate(1, 0, th)), True)
+        fn(*translate(x, y, *rotate(2, 0, th)), False)
+        fn(*translate(x, y, *rotate(0, 1, th)), False)
 
     @staticmethod
     def b(x, y, th, fn):
         """
         ..b
-        Bbb
+        Bbx
         """
-        fn(x, y)
-        fn(*translate(x, y, *rotate(0, 1, th)))
-        fn(*translate(x, y, *rotate(0, 2, th)))
-        fn(*translate(x, y, *rotate(1, 2, th)))
+        fn(x, y, False)
+        fn(*translate(x, y, *rotate(1, 0, th)), False)
+        fn(*translate(x, y, *rotate(2, 0, th)), True)
+        fn(*translate(x, y, *rotate(2, 1, th)), False)
 
     @staticmethod
     def c(x, y, th, fn):
         """
         .c.
-        Ccc
+        Cxc
         """
-        fn(x, y)
-        fn(*translate(x, y, *rotate(0, 1, th)))
-        fn(*translate(x, y, *rotate(0, 2, th)))
-        fn(*translate(x, y, *rotate(1, 1, th)))
+        fn(x, y, False)
+        fn(*translate(x, y, *rotate(1, 0, th)), True)
+        fn(*translate(x, y, *rotate(2, 0, th)), False)
+        fn(*translate(x, y, *rotate(1, 1, th)), False)
 
     @staticmethod
     def d(x, y, th, fn):
         """
-        ..d
+        ..x
         Ddd
-        .d.
+        .x.
         """
-        fn(x, y)
-        fn(*translate(x, y, *rotate(0, 1, th)))
-        fn(*translate(x, y, *rotate(0, 2, th)))
-        fn(*translate(x, y, *rotate(1, 2, th)))
-        fn(*translate(x, y, *rotate(-1, 1, th)))
+        fn(x, y, False)
+        fn(*translate(x, y, *rotate(1, 0, th)), False)
+        fn(*translate(x, y, *rotate(2, 0, th)), False)
+        fn(*translate(x, y, *rotate(2, 1, th)), True)
+        fn(*translate(x, y, *rotate(1, -1, th)), True)
 
 class Board:
-    def __init__(self):
-        self.occ = {}
+    def __init__(self, occ=None, history=None):
+        self.occ = occ.copy() if occ else {}
+        self.history = history.copy() if history else []
+
+    def place_cat(self, col, row):
+        if (col, row) in self.occ:
+            raise Violation('collision with something')
+        self.occ[(col, row)] = '+'
 
     def place_block(self, col, row, theta, kind):
-        def pop(x, y):
+        def pop(x, y, box):
             if x < 0 or x > 4 or y < 0 or y > 4:
                 raise Violation('out of bounds')
             if (x, y) in self.occ:
-                raise Violation('collision with something')
+                cat_in_a_box = box and self.occ[(x, y)] == '+'
+                if not cat_in_a_box:
+                    raise Violation('collision with something')
             self.occ[(x, y)] = kind
-        getattr(Gen, kind)(col, row, theta, pop)
+        try:
+            self.history.append((col, row, theta, kind))
+            getattr(Gen, kind)(col, row, theta, pop)
+            return True
+        except Violation:
+            return False
+
+    def clone(self):
+        return Board(self.occ, self.history)
 
     def __repr__(self):
         lines = []
@@ -102,39 +118,34 @@ class Board:
 
 placements = [(x, y, t) for x in range(5) for y in range(5) for t in range(4)]
 
-v = []
+vs = []
+x = Board()
+x.place_cat(0, 4)
+x.place_cat(0, 1)
+x.place_cat(2, 3)
+x.place_cat(4, 3)
+x.place_cat(4, 0)
+print(x)
 for p1 in placements:
-    for p2 in placements:
-        for p3 in placements:
-            for p4 in placements:
-                try:
-                    a = Board()
-                    a.place_block(*p1, 'a')
-                    a.place_block(*p2, 'b')
-                    a.place_block(*p3, 'c')
-                    a.place_block(*p4, 'd')
-                    v.append(a)
-                except Violation as e:
-                    pass
-print_boards(v)
+    a = x.clone()
+    if a.place_block(*p1, 'a'):
+        for p2 in placements:
+            b = a.clone()
+            if b.place_block(*p2, 'b'):
+                for p3 in placements:
+                    c = b.clone()
+                    if c.place_block(*p3, 'c'):
+                        for p4 in placements:
+                            d = c.clone()
+                            if d.place_block(*p4, 'd'):
+                                vs.append(d)
+print_boards(vs)
+print(f'solutions: {len(vs)}')
+print()
 
-# print(a)
-
-# a = Block(0, 0, 'a', 2)
-# if a:
-#     print(a)
-
-# a = Block(3, 2, 'a', 2)
-# print(a)
-
-# import pdb;pdb.set_trace()
-
-# a.occ
-
-    # def __repr__():
-    #     pass
-
-
+for v in vs[:5] + vs[90:92]:
+    print(v.history)
+    print(v)
 
 # import graphviz
 # dot = graphviz.Digraph(comment='The Round Table')
