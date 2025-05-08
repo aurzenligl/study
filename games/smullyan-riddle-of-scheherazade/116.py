d31f1c2ef50b5e428779742267e33f8b105470be
Krzysztof Laskowski <aurzenligl@gmail.com>
2025-05-08 21:38:15 +0200

    smullyan 116 next part

--- games/smullyan-riddle-of-scheherazade/116.py
+++ games/smullyan-riddle-of-scheherazade/116.py
@@ -18,6 +18,22 @@ def princess(value: str):
     if value.startswith('4'):
         return princess(value[1:])[::-1]
 
+    # rule 4:
+    # princess(x) = y  =>  princess('5' + x) = y[1:], when len(y) >= 2
+    if value.startswith('5'):
+        y = princess(value[1:])
+        if len(y) < 2:
+            raise Exception('princess cannot subtract digit from number with less than two digits')
+        return y[1:]
+
+    # rule 5:
+    # princess(x) = y  =>  princess('6' + x) = '1' + y
+    # princess(x) = y  =>  princess('7' + x) = '2' + y
+    if value.startswith('6'):
+        return '1' + princess(value[1:])
+    if value.startswith('7'):
+        return '2' + princess(value[1:])
+
     raise Exception('princess does not understand the number')
 
 value = sys.argv[1]
