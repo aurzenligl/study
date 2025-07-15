f07feca50c8129d659086ecba66a88cda6f18861
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2025-07-15 19:26:46 +0200

    tunic: default phoneme substitutions built in

--- games/tunic/tuniclang.py
+++ games/tunic/tuniclang.py
@@ -9,6 +9,60 @@ from pathlib import Path
 from typing import List
 
 
+BUILTIN_OUTIES = {
+    '11100': 'æ',   # and, grant, shadow, cast, that
+    '01100': 'ɔː',  # sought, gnaw, all
+    '00011': 'ɪ',   # in, is, live, ruin
+    '00111': 'e',   # open, held, self, thread
+    '00110': 'ʊ',   # look, understood, faithful
+    '11000': 'ə',   # but, one, must
+
+    '01111': 'i',   # between, holy, seek, be
+    '11110': 'u',   # to, you, truth, tomb
+    '10111': 'əʳ',  # power, nature, manner, seeker, were
+    '11101': 'ɔːʳ', # shore, your, or, for, glory
+    '11011': 'ɑːʳ', # far, apart, arc, sarcophagus
+    '01101': 'ɪəʳ', # here, disappear, hero
+
+    '01000': 'eɪ',  # wait, same, break, plain, great
+    '10000': 'aɪ',  # lie, outside, arrive, life
+    '00010': 'ɔɪ',  # coil, spoil, turmoil, boil
+    '00001': 'aʊ',  # about, outside, power
+    '11111': 'oʊ',  # no, open, hole, old (UK: əʊ)
+    '00101': 'eəʳ', # their, heir, careful, bear
+}
+
+BUILTIN_INNIES = {
+    '000101': 'm',  # from, become, wisdom
+    '001101': 'n',  # and, plain, one
+    '111111': 'ŋ',  # ring, seeking, alluring
+    '100010': 'p',  # plain, palace, open
+    '010001': 'b',  # build, become, but
+    '101010': 't',  # to, tomb, sought, built
+
+    '010101': 'd',  # do, discover, and, lived
+    '110001': 'k',  # cast, secret, sacred, seek
+    '100011': 'g',  # grow, glory, begin
+    '010100': 'dʒ', # origin, gene, jam
+    '001010': 'tʃ', # which, nature, venture, future
+    '100110': 'f',  # for, fable, life, self
+
+    '011001': 'v',  # give, leave, of, cover
+    '111010': 'θ',  # thread, thunder, truth, with
+    '010111': 'ð',  # the, they, there, those, this
+    '110110': 's',  # so, cycle, seeker, quest, curse
+    '011011': 'z',  # those, lives, as, is, was
+    '101111': 'ʃ',  # shore, shatter, sure, constradiction, potential
+
+    '111101': 'ʒ',  # usual, treasure, vision
+    '010011': 'h',  # have, holy, held, here, perhaps
+    '110010': 'r',  # real, reveal, ruin, true
+    '011010': 'j',  # you, new, future, continue
+    '101000': 'w',  # will, wait, were, quest
+    '010010': 'l',  # long, life, will, hole
+}
+
+
 # https://en.wikipedia.org/wiki/Letter_frequency
 PAT = re.compile('[01]{6}:[01]{5}:[01]')
 PAT3 = re.compile('[01]{6}:[01]{5}:[01]')
@@ -129,20 +183,21 @@ def parse_opts():
     parser = argparse.ArgumentParser(prog='tuniclang-hacker')
     parser.add_argument('filename')
     parser.add_argument('-c', '--count', action='store_true')
+    parser.add_argument('-s', '--substitute', metavar='TUNICCODE:LETTER', action='append', default=[])
+    parser.add_argument('-u', '--underscore', metavar='TUNICCODE', action='append', default=[])
     parser.add_argument('-r', '--raw', action='store_true')
-    parser.add_argument('-s', '--substitute', metavar='TUNICCODE:LETTER', action='append')
-    parser.add_argument('-u', '--underscore', metavar='TUNICCODE', action='append')
     return parser.parse_args()
 
 
 def main():
     opts = parse_opts()
+    ibltins = [f'i{code}:{letter}' for code, letter in BUILTIN_INNIES.items()]
+    obltins = [f'o{code}:{letter}' for code, letter in BUILTIN_OUTIES.items()]
+    opts.substitute = ibltins + obltins + opts.substitute
     if opts.count:
         cmd_count(Path(opts.filename))
-    elif opts.substitute:
-        cmd_substitute(Path(opts.filename), opts.substitute, opts.underscore or [], opts.raw)
     else:
-        sys.exit('error: no operation chosen')
+        cmd_substitute(Path(opts.filename), opts.substitute, opts.underscore or [], opts.raw)
 
 
 if __name__ == '__main__':
