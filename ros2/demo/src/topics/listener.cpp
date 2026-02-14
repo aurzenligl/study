572bd74d4ef11c8a16604e0bd42ad0c583516db4
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2026-02-14 00:19:45 +0000

    ros2 talker/listener test

---
+++ ros2/demo/src/topics/listener.cpp
@@ -0,0 +1,32 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char **argv) {
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);  // Force flush of the stdout buffer.
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("listener");
+
+  auto t0 = std::chrono::steady_clock::now();
+  int count = 0;
+
+  auto callback = [&](std_msgs::msg::String::ConstSharedPtr) {
+    ++count;
+    auto t1 = std::chrono::steady_clock::now();
+    const std::chrono::duration<double> d1 = t1 - t0;
+    if (d1.count() >= 1) {
+      double freq = count / d1.count();
+      t0 = t1;
+      count = 0;
+      RCLCPP_INFO(node->get_logger(), "Frequency: %.3f [Hz]", freq);
+    }
+
+    // RCLCPP_INFO(node->get_logger(), "I heard: [%s]", msg->data.c_str());
+  };
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub =
+      node->create_subscription<std_msgs::msg::String>("chatter", 10, callback);
+  rclcpp::spin(node);
+  return 0;
+}
---
+++ ros2/demo/src/topics/talker.cpp
@@ -0,0 +1,30 @@
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char **argv) {
+  setvbuf(stdout, NULL, _IONBF, BUFSIZ);  // Force flush of the stdout buffer.
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("talker");
+
+  size_t count = 1;
+  rclcpp::QoS qos(rclcpp::KeepLast{7});
+  auto pub = node->create_publisher<std_msgs::msg::String>("chatter", qos);
+
+  auto publish_message = [&]() {
+    auto msg = std::make_unique<std_msgs::msg::String>();
+    msg->data = "Hello World: " + std::to_string(count++);
+    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", msg->data.c_str());
+    pub->publish(std::move(msg));
+  };
+
+  while (rclcpp::ok()) {
+    publish_message();
+  }
+
+  // auto timer = node->create_wall_timer(1s, publish_message);
+  // rclcpp::spin(node);
+
+  return 0;
+}
