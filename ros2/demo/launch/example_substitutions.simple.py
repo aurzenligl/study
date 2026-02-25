20c6164d65a3d1685f9de3394476a31a735bc47d
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2026-02-25 00:08:40 +0000

    ros2 demo exercise in writing launch files

---
+++ ros2/demo/launch/example_substitutions.simple.py
@@ -0,0 +1,13 @@
+from simple_launch import SimpleLauncher
+
+
+def launch(sl: SimpleLauncher):
+    background_r = 200
+    sl.include("demo", "example_substitutions.simple.py.include", launch_arguments={
+        "turtlesim_ns": "turtlesim2",
+        "use_provided_red": True,
+        "new_background_r": background_r,
+    })
+
+
+generate_launch_description = lambda: launch(sl := SimpleLauncher()) or sl.launch_description()
